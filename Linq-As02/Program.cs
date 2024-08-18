a867276d8c3229dc9bc001a68c006aca3678e6e3
nadinegawad <nadinerifaat00@gmail.com>
2024-08-18 11:00:50 -0700

    Add project files.

---
+++ Linq-As02/Program.cs
@@ -0,0 +1,278 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using static Linq_As02.ListGenertor;
+namespace Linq_As02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region LINQ - Restriction Operators
+            #region Q01
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0);
+            #endregion
+
+            #region Q02
+
+            //var Result = ProductsList.Where(P => P.UnitsInStock> 0&& P.UnitPrice>3);
+
+            #endregion
+
+            #region Q03
+
+            #endregion
+            #endregion
+
+            #region LINQ - Element Operators
+            #region Q01
+            //var Result = ProductsList.FirstOrDefault();
+            #endregion
+
+            #region Q02
+            //var Result = ProductsList.FirstOrDefault(P =>P.UnitPrice>1000);
+            #endregion
+
+            #region Ex03
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Where(P => P > 5).ElementAtOrDefault(1); 
+            #endregion
+            #endregion
+
+            #region LINQ - Aggregate Operators
+            #region Q01
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Where(N => N % 2 != 0).Count();
+            #endregion
+
+            #region Q02
+            //var Result = CustomersList.SelectMany(C => C.Orders).Count();
+            #endregion
+
+            #region Q03
+            //var Result = ProductsList.Select((P, C) => new
+            //{
+            //    P.Category,
+            //    Count= P.Category.Count()
+            //});
+            //Result = Result.Distinct(); 
+            #endregion
+            #region Q04
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Sum(); 
+            #endregion
+            #region Q05
+            //string[] str = File.ReadAllLines("dictionary_english.txt");
+            //var Result = str.Select(S => S.Length).Sum(); 
+            #endregion
+            #region Q06
+            //string[] str = File.ReadAllLines("dictionary_english.txt");
+            //var Result = str.Select(S => S.Length).Min();
+            #endregion
+
+            #region Q07
+            //string[] str = File.ReadAllLines("dictionary_english.txt");
+            //var Result = str.Select(S => S.Length).Max();
+            #endregion
+            #region Q08
+            //string[] str = File.ReadAllLines("dictionary_english.txt");
+            //var Result = str.Select(S => S.Length).Average(); 
+            #endregion
+
+            #region Q09
+            //var Result = ProductsList.Select(P => new
+            //{
+            //    P.Category,
+            //    P.UnitsInStock
+            //}); 
+            #endregion
+            #region Q10
+            //var Result = ProductsList.Select(P => new
+            //{
+            //    P.Category,
+            //    P.UnitPrice
+            //});
+            #endregion
+
+            #endregion
+
+
+            #region LINQ - Ordering Operators
+
+            #region Q01
+            //var Result = ProductsList.OrderBy(P => P.ProductName);
+            #endregion
+            #region Q03
+            //var Result = ProductsList.OrderByDescending(P => P.UnitsInStock);
+
+            #endregion
+
+            #region Q04
+            //string[] Arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+            //var Result = Arr.OrderBy(P => P.Length).ThenBy(P=>P);
+            #endregion
+            #region Q05
+            //String[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //var Result = Arr.OrderBy(P => P.Length).ThenBy(word => word.ToLower());
+            #endregion
+
+            #region Q06
+            //var Result = ProductsList.OrderBy(P => P.Category).ThenByDescending(P => P.UnitPrice);
+            #endregion
+            #region Q07
+            //  String[] Arr = { "aPPLE", "AbAcUs", "bRaNcH", "BlUeBeRrY", "ClOvEr", "cHeRry" };
+            //  var Result = Arr
+            //.OrderBy(word => word.Length) 
+            //.ThenByDescending(word => word.ToLower());
+            #endregion
+
+
+            #endregion
+
+            #region LINQ – Transformation Operators
+
+            #region Q01
+            //var Result = ProductsList.Select(P => P.ProductName);
+            #endregion
+            #region Q02
+            //String[] words = { "aPPLE", "BlUeBeRrY", "cHeRry" };
+            //var Result = words.Select(W => new
+            //{
+            //    Original = W,
+            //    Upper = W.ToUpper(),
+            //    Lower = W.ToLower(),
+            //});
+            #endregion
+            #region Q03
+            //var Result = ProductsList.Select(P => new
+            //{
+            //    P.ProductName,
+            //    P.UnitsInStock,
+            //    Price = P.UnitPrice
+            //});
+            #endregion
+
+            #region Q04
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = Arr.Select((P, I) => new
+            //{
+            //    index = I,
+            //    number = P
+            //}).Where(P => P.index == P.number);
+
+            #endregion
+
+            #region Q05
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
+            //var Result = from A in numbersA
+            //             from B in numbersB
+            //             where A > B
+            //             select new {A,B};
+            //foreach (var item in Result)
+            //{
+            //    Console.WriteLine($"{item.A} is less than {item.B}");
+            //}
+            #endregion
+
+            #region Q06
+            //var Result = CustomersList.SelectMany(C => C.Orders);
+            #endregion
+
+            #region Q07
+            //var Result = CustomersList.SelectMany(C => C.Orders).Where(P=>P.OrderDate.Date.Year>= 1998);
+
+            #endregion
+
+            #endregion
+
+            #region LINQ - Set Operators
+            #region Q01
+            //var Result = ProductsList.Select(P => P.Category).Distinct();
+            #endregion
+
+            #region Q02
+            //var PName = ProductsList.Select(P => P.ProductName).FirstOrDefault();
+            //var CName = CustomersList.Select(C => C.CustomerName).FirstOrDefault();
+            //var Result=PName.Concat(CName);
+            #endregion
+
+            #region Q03
+            //var PName = ProductsList.Select(P => P.ProductName).FirstOrDefault();
+            //var CName = CustomersList.Select(C => C.CustomerName).FirstOrDefault();
+            //var Result = PName.Intersect(CName);
+            #endregion
+            #region Q04
+            //var PName = ProductsList.Select(P => P.ProductName).FirstOrDefault();
+            //var CName = CustomersList.Select(C => C.CustomerName).FirstOrDefault();
+            //var Result = PName.Except(CName);
+            #endregion
+            #region Q05
+            //var PName = ProductsList.Select(P => P.ProductName.Length >= 3 ? P.ProductName.Substring(P.ProductName.Length - 3) : P.ProductName);
+            //var CName = CustomersList.Select(C => C.CustomerName.Length >= 3 ? C.CustomerName.Substring(C.CustomerName.Length - 3) : C.CustomerName);
+            //var Result = PName.Except(CName);
+            #endregion
+            #endregion
+
+            #region LINQ - Partitioning Operators
+            #region Q01
+            //var Result = CustomersList.Where(C => C.City == "Washington").Take(5); 
+            #endregion
+
+            #region Q02
+            //var Result = CustomersList.Where(C => C.City == "Washington").SelectMany(C=>C.Orders).Skip(2);
+
+            #endregion
+
+            #region Q03
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            #endregion
+
+            #region Q4
+            //int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var Result = numbers.TakeWhile(N=>N%3==0);
+            #endregion
+            #region Q5
+            //  int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //  var Result = numbers
+            //.Select((value, index) => new { value, index })
+            //.SkipWhile(x => x.value >= x.index)
+            //.Select(x => x.value);
+
+            #endregion
+            #endregion
+
+            #region LINQ - Quantifiers
+            #region Q01
+            //string[] str = File.ReadAllLines("dictionary_english.txt");
+            //var Result = str.Any(word => word.Contains("ei")); 
+            #endregion
+            #region Q02
+            //var Result = ProductsList.Where(P => P.UnitsInStock >= 1).Select(P => P.Category).Distinct();
+            #endregion
+            #region Q03
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 0).Select(P => P.Category).Distinct();
+            #endregion
+
+            #endregion
+
+            #region LINQ – Grouping Operators
+            #region Q01
+            //List<int> numbers = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            //var Result = numbers.GroupBy(N => N % 5);
+            #endregion
+            #region Q02
+
+            #endregion
+            #endregion
+            //Console.WriteLine(Result);
+            foreach (var Product in Result)
+            {
+                Console.WriteLine(Product);
+            }
+
+        }
+    }
+}
---
+++ Linq02-Demo/Program.cs
@@ -0,0 +1,269 @@
+﻿using System.Collections;
+using System.Net.WebSockets;
+using System.Text.RegularExpressions;
+using static Linq02_Demo.ListGenertor;
+
+namespace Linq02_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            //Console.WriteLine(ListGenertor.ProductsList[0]);
+            //Console.WriteLine(ListGenertor.CustomersList[0]);
+
+            #region Filteration(where)
+
+            #region Ex01
+            //fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0);
+            //query
+            //Result = from P in ProductsList
+            //         where P.UnitsInStock == 0
+            //         select P;
+
+            #endregion
+            #region Ex02
+            // fluent
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0 && P.Category == "Condiments");
+            //query
+            //Result = from P in ProductsList
+            //         where P.UnitsInStock == 0 && P.Category == "Condiments"
+            //         select P; 
+            #endregion
+            #region Ex03
+            //var Result = ProductsList.Where((P, I) => I < 10 && P.UnitsInStock == 0); 
+            #endregion
+            #endregion
+
+            #region Transformation(Select-SelectMany)
+            #region Ex01
+            //var Result = ProductsList.Select(P => P.ProductName);
+            //Result = from P in ProductsList
+            //         select P.ProductName; 
+            #endregion
+            #region Ex02
+
+            //var Result = CustomersList.SelectMany(C => C.Orders);
+            //Result = from C in CustomersList
+            //         from O in C.Orders
+            //         select O; 
+            #endregion
+            #region Ex03
+            //var Result = ProductsList.Select(P => new
+            //{
+            //     P.ProductID,
+            //     P.ProductName
+            //});
+            //Result = from P in ProductsList
+            //         select new
+            //         {
+            //             P.ProductID,
+            //             P.ProductName
+            //         }; 
+            #endregion
+            #region Ex04
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0)
+            //                         .Select(P => new
+            //                         {
+            //                             P.ProductID,
+            //                             P.ProductName,
+            //                             OldPrice = P.UnitPrice,
+            //                             NewPrice = P.UnitPrice * 0.90M
+            //                         }); 
+            #endregion
+            #region Ex05
+            //var Result = ProductsList.Select((P, I) => new
+            //{
+            //    Index = I,
+            //    P.ProductName
+            //}); 
+            #endregion
+            #endregion
+
+            #region Ording
+
+
+            #region Ex01
+            //var Result = ProductsList.OrderBy(P => P.UnitPrice);
+            // //Result = ProductsList.OrderByDescending (P => P.UnitPrice);
+            //Result = from P in ProductsList
+            //         orderby P.UnitPrice descending
+            //         select P; 
+            #endregion
+            #region EX02
+            //var Result = ProductsList.OrderBy(P => P.UnitPrice)
+            //                         .ThenByDescending (P => P.UnitsInStock); 
+            #endregion
+
+            #region Ex03
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0).Reverse();
+
+            #endregion
+            #endregion
+
+            #region Elements
+
+            #region Ex01
+            //var Result = ProductsList.First();
+            // Result = ProductsList.Last();
+            //Result = ProductsList.FirstOrDefault();
+            //Result = ProductsList.LastOrDefault(); 
+            #endregion
+            #region Ex02
+            //var Result = ProductsList.First(P =>P.UnitsInStock==0);
+            // Result = ProductsList.FirstOrDefault(P =>P.UnitsInStock==0); 
+            #endregion
+            #region Ex03
+            //var Result = ProductsList.ElementAt(0);
+            // Result = ProductsList.ElementAtOrDefault (0); 
+            #endregion
+            #region Ex04
+            //var Result = ProductsList.Single();
+            //var Result = ProductsList.Single(P => P.UnitPrice == 0);
+            // Result = ProductsList.SingleOrDefault(P => P.UnitPrice == 0); 
+            #endregion
+
+            #region Ex05
+            //var Result = (from P in ProductsList
+            //              where P.UnitsInStock == 0
+            //              select new
+            //              {
+            //                  P.ProductID,
+            //                  P.ProductName,
+            //                  P.UnitPrice
+            //              }).FirstOrDefault(); 
+            #endregion
+            #endregion
+
+            #region Aggregate
+            #region Count
+            //var Result = ProductsList.Count();
+            //var Result = ProductsList.Count;
+            //var Result = ProductsList.Count(P =>P.UnitsInStock==0); 
+            #endregion
+            #region Max-Min
+            //var Result = ProductsList.Max();
+            //Result = ProductsList.Min();
+            //var Result = ProductsList.Max(P => P.ProductName.Length); 
+            #endregion
+            #region Sum -Average
+            //var Result = ProductsList.Sum(P => P.UnitPrice);
+            //var Result = ProductsList.Average(P => P.UnitPrice);
+            #endregion
+            #endregion
+
+            #region Casting
+            //var Result = ProductsList.Where(P => P.UnitsInStock==0).ToList();
+            //var Result = ProductsList.Where(P => P.UnitsInStock==0).ToArray();
+            //var Result = ProductsList.ToDictionary(P => P.ProductID);
+            //var Result = ProductsList.ToDictionary(P => P.ProductID, P =>P.ProductName);
+            //var Result = ProductsList.Where(P => P.UnitsInStock == 0).ToHashSet();
+
+            //ArrayList arr = new ArrayList() { "Ahmed", "amr", 9, 3, 2, 7, "aya" };
+            //var Result = arr.OfType<int>(); 
+            #endregion
+
+            #region Generation
+            //var Result = Enumerable.Range(1, 100);
+            //var Result = Enumerable.Repeat(2, 50);
+            //var Result= Enumerable.Empty<Product>(); 
+            #endregion
+
+            #region Set
+            //var Sq01 = Enumerable.Range(1, 100);
+            //var Sq02 = Enumerable.Range(50, 100);
+            //var Result = Sq01.Union(Sq02);
+            //var Result = Sq01.Concat(Sq02);
+            //var result= Result.Distinct();
+            //var Result = Sq01.Intersect(Sq02);
+            //var Result = Sq01.Except(Sq02); 
+            #endregion
+
+            #region Quentifier
+            //var Result= ProductsList.Any();
+            // Result= ProductsList.Any(P =>P.UnitPrice>500);
+            // Result= ProductsList.All(P =>P.UnitPrice>3); 
+            #endregion
+
+            #region Zip
+            //string[] Names = { "Ahmed", "amr", "Aya", "omar", "Kareem" };
+            //int[] NUmbers = { 1, 2, 3, 4, 5 };
+            //char[] Chars = { 'a', 'b', 'c', 'd' };
+
+            //var Result = Names.Zip(NUmbers);
+            //var Result = Names.Zip(NUmbers, Chars);
+            //var Result = Names.Zip(NUmbers, (I, N) => new
+            //{
+            //    I=NUmbers,
+            //    N=Names
+            //}); 
+            #endregion
+
+            #region Group By
+            #region Ex01
+            //var Result = from P in ProductsList
+            //             group P by P.Category;
+
+            //Result = ProductsList.GroupBy(P => P.Category); 
+            #endregion
+
+            #region Ex02
+            //var Result = from P in ProductsList
+            //             where P.UnitsInStock > 10
+            //             group P by P.Category
+            //             into C
+            //             where C.Count() > 10
+            //             select C;
+            //var Result = ProductsList.Where(P => P.UnitsInStock > 10).GroupBy(P => P.Category).Where(P => P.Count() > 10); 
+            #endregion
+            #endregion
+
+            #region Partition
+            //var Result = ProductsList.Take(10);
+            //var Result = ProductsList.Skip(10);
+            //var Result = ProductsList.TakeLast(10);
+            //var Result = ProductsList.SkipLast(10);
+
+            //int[] Numbers = { 1, 8, 7, 9, 3, 2, 5, 4, 6, 8 };
+            //var Result = Numbers.TakeWhile(N => N <9);
+            //var Result = Numbers.SkipWhile(N => N <9); 
+            #endregion
+
+            #region Let and Into
+            //string[] Names = { "Ahmes", "OMar", "aya", "maye", "Kareem" };
+
+            //var Result = from N in Names
+            //             select Regex.Replace(N, "[AEOIUaeiou]", string.Empty)
+            //             into Vowel
+            //             where Vowel.Length > 0
+            //             select Vowel; 
+            //using let
+            //var Result = from N in Names
+            //             let Vowel= Regex.Replace(N, "[AEOIUaeiou]", string.Empty)
+            //             where Vowel.Length > 0
+            //             select Vowel;
+            #endregion
+
+
+
+            //foreach (var item in Result)
+            //{
+            //    Console.Write($"{item} ");
+            //}
+            //Console.WriteLine(Result);
+            //foreach (var Product in Result)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+            //foreach (var Category in Result)
+            //{
+            //    Console.WriteLine(Category.Key);
+            //    foreach(var Product in Category)
+            //    {
+            //        Console.WriteLine(Product);
+            //    }
+            //}
+        }
+    }
+}
